c04f835e7a8031bc6e984d1cf279b455fd792ae3
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2023-11-06 01:25:03 +0530

    added navbar component and made it responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Theme } from '@radix-ui/themes';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Theme>
     <App />
+    </Theme>
+
   </React.StrictMode>,
 )
